138d432f807059f6b62dfa90b80a021d1fcfc879
SauceArgente <silversocce@gmail.com>
2021-02-16 18:02:29 +0100

    Added hooks

--- client-test/index.js
+++ client-test/index.js
@@ -1,31 +1,46 @@
 let jason = require("jasonbase")
 let cities = require("./popualtion")
 
-let db = new jason.db({url:"localhost"})
-
-//cities.forEach(data => {
-//    db.collection("population").doc(data.country.toLowerCase()).writeFile({
-//        population:data.population,
-//        country:data.country.toLowerCase()
-//    })
-//});
-
-db.collection("population").query({
-    allInstances:{
-        where:{
-            country:{
-                equals:"spain"
-            }
-        }
-    }
-}).getOnce(data=>{
-    console.log("France's data : " + JSON.stringify(data[0]))
+let db = new jason.db({
+    url:"localhost",
+    key:"Bjn02NFZBVwXUjpB"
 })
 
+cities.forEach(data => {
+    db.collection("population").doc(data.country.toLowerCase()).writeFile({
+        population:data.population,
+        country:data.country.toLowerCase()
+    }).then(result=>{
 
-/*db.doc("w").getOnce(data=>{
-    console.log("Fetched city : " + data.city)
-})*/
+    }).catch(error=>{
+        console.log(error)
+    })
+});
+
+//let targetNum = Math.pow(10,8);
+//
+//db.collection("population").query({
+//    allInstances:{
+//        where:{
+//            population:{
+//                greater:targetNum
+//            }
+//        }
+//    }
+//}).getOnce().then(data=>{
+//    console.log(`All the countries with more than ${targetNum} habitants`)
+//    data.forEach(cd => {
+//        console.log(`--> ${cd.country} with ${cd.population} habitants`)
+//    });
+//})
+
+//
+//db.collection("population").doc("spain").getOnce().then(data=>{
+//    console.log(JSON.stringify(data))
+//}).catch(error=>{
+//    console.log("Error : " + error)
+//})
+
 
 /*db.doc("w").getRealTime(data=>{
     console.log(data)
